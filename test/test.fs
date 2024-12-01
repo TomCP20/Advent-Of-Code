3468243d68bf72c1d9d0ae0619233410e4dc5e91
dei20 <107045601+TomCP20@users.noreply.github.com>
2024-12-01 00:38:02 +0000

    added example F# code

---
+++ test/test.fs
@@ -0,0 +1,6 @@
+﻿open System
+
+let read _ = Console.ReadLine()
+let isValid = function null -> false | _ -> true
+let inputList = Seq.initInfinite read |> Seq.takeWhile isValid |> Seq.toList
+printfn "%A" inputList
