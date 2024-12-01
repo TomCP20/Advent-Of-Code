b9d4b7e72f9352692af23f805fff768c34359993
dei20 <107045601+TomCP20@users.noreply.github.com>
2024-12-01 21:54:02 +0000

    added f# solution for day 1

---
+++ 2024/Day 1/F#/Program.fs
@@ -0,0 +1,13 @@
+﻿open System
+
+let read _ = Console.ReadLine()
+let isValid = function null -> false | _ -> true
+let inputList: string list = Seq.initInfinite read |> Seq.takeWhile isValid |> Seq.toList
+let lists = [ for item: string in inputList -> item.Split "   "] 
+let left = [ for item in lists -> int(item[0])]
+let right = [ for item in lists -> int(item[1])]
+
+printfn "%A" (List.sumBy (fun (l, r) -> abs (l-r)) (List.zip (List.sort left) (List.sort right)))
+
+let count =  right |> Seq.countBy id |> Map.ofSeq
+printfn "%A" (List.sumBy (fun x -> x*(count.TryFind(x) |> Option.defaultValue 0)) left)
