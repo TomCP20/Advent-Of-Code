82f0346b6abddabe27d9629a6dcb225aec934dcf
dei20 <107045601+TomCP20@users.noreply.github.com>
2024-12-01 07:19:13 +0000

    refactored part 1

--- 2024/Day 1/Python/p1.py
+++ 2024/Day 1/Python/p1.py
@@ -1,7 +1,5 @@
 input = open(0).read().splitlines()
 lists = [line.split("   ") for line in input]
-left = [int(line[0]) for line in lists]
-right = [int(line[1]) for line in lists]
-left.sort()
-right.sort()
+left = sorted(int(line[0]) for line in lists)
+right = sorted(int(line[1]) for line in lists)
 print(sum(abs(l-r) for (l, r) in zip(left, right)))
