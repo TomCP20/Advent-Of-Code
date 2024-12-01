9e71055ad18c3358a70bef4c33982cb014d685af
dei20 <107045601+TomCP20@users.noreply.github.com>
2024-12-01 20:53:39 +0000

    refactored rust program

--- 2024/Day 1/Rust/src/main.rs
+++ 2024/Day 1/Rust/src/main.rs
@@ -1,8 +1,8 @@
+use std::io;
 use itertools::Itertools;
-use std::{collections::HashMap, convert::TryFrom};
 
 fn main() {
-    let lines = std::io::stdin().lines().collect::<Result<Vec<String>, std::io::Error>>().unwrap();
+    let lines = io::stdin().lines().collect::<Result<Vec<String>, io::Error>>().unwrap();
     let rows: Vec<Vec<&str>> = lines.iter().map(|x| x.split("   ").collect::<Vec<_>>()).collect();
     let mut left: Vec<i32> = Vec::new();
     let mut right: Vec<i32> = Vec::new();
@@ -15,13 +15,6 @@ fn main() {
     let result1: i32 = left.iter().zip(right.iter()).map(|(l, r)| i32::abs(l-r)).sum();
     println!("{}", result1);
     let counter = right.into_iter().counts();
-    let result2: i32 = left.into_iter().map(|x| x*get_count(&counter, x)).sum();
+    let result2: usize = left.into_iter().map(|x| x as usize * counter.get(&x).unwrap_or(&0)).sum();
     println!("{}", result2);
-}
-
-fn get_count(counter: &HashMap<i32, usize>, key: i32) -> i32 {
-    return match counter.get(&key) {
-        None => 0,
-        Some(count) => i32::try_from(count.clone()).unwrap(),
-    };
 }
