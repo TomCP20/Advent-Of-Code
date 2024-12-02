c61c5b676666482bda6cc6f934bd641b7f6258ff
dei20 <107045601+TomCP20@users.noreply.github.com>
2024-12-02 02:54:30 +0000

    added c# solution for day 1

---
+++ 2024/Day 1/C#/Program.cs
@@ -0,0 +1,14 @@
+﻿List<string> lines = [];
+string? line;
+while ((line = Console.ReadLine()) != null)
+{
+    lines.Add(line);
+}
+List<List<int>> lists = lines.Select(x => x.Split("   ").Select(x => int.Parse(x)).ToList()).ToList();
+List<int> left = [.. lists.Select(x => x[0]).Order()];
+List<int> right = [.. lists.Select(x => x[1]).Order()];
+
+Console.WriteLine(left.Zip(right, (l, r) => Math.Abs(l - r)).Sum());
+Dictionary<int, int> count = right.GroupBy(x => x).ToDictionary(g => g.Key, g => g.Count());
+int sum = left.Where(count.ContainsKey).Select(x => x*count[x]).Sum();
+Console.WriteLine(sum);
