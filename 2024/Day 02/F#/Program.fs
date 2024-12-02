17c52c109e1b89b5bbc4cddf6f283e38ce5adb71
dei20 <107045601+TomCP20@users.noreply.github.com>
2024-12-02 19:14:58 +0000

    added day 2 solution for F#

--- 2024/Day 02/F#/Program.fs
+++ 2024/Day 02/F#/Program.fs
@@ -1,2 +1,12 @@
-﻿// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+﻿open System
+
+let read _ = Console.ReadLine()
+let isValid = function null -> false | _ -> true
+let isSafe (report: int seq) = report |> Seq.pairwise |> Seq.map (fun (a, b) -> b-a) |> (fun s -> Seq.forall (fun x -> 1 <= x && x <= 3) s || Seq.forall (fun x -> 1 <= -x && -x <= 3) s)
+let inputList: string array = Seq.initInfinite read |> Seq.takeWhile isValid |> Seq.toArray
+let reports = inputList |> Array.map(fun x -> x.Split(" ") |> Array.map(int))
+
+printfn "%A" (reports |> Seq.filter isSafe |> Seq.length)
+
+let isSafeish (report: int array) = isSafe report || seq {0 .. report.Length - 1} |> Seq.map (fun i -> report |> Array.mapi (fun ri x -> (ri, x)) |> Array.filter (fun (ri, x) -> ri<>i) |> Array.map (fun (ri, x) -> x)) |> Seq.exists isSafe
+printfn "%A" (reports |> Seq.filter isSafeish |> Seq.length)
