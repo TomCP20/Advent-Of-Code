fbaaf0149676724b64685226e1807a3150c01f8b
dei20 <107045601+TomCP20@users.noreply.github.com>
2024-12-02 03:02:44 +0000

    removed test folder

--- test/test.fs
+++
@@ -1,6 +0,0 @@
-﻿open System
-
-let read _ = Console.ReadLine()
-let isValid = function null -> false | _ -> true
-let inputList = Seq.initInfinite read |> Seq.takeWhile isValid |> Seq.toList
-printfn "%A" inputList
--- test/test.rs
+++
@@ -1,4 +0,0 @@
-fn main() {
-    let lines = std::io::stdin().lines().collect::<Result<Vec<String>, std::io::Error>>().unwrap().join("\n");
-    println!("{}", lines);
-}
