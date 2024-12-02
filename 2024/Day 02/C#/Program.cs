9c48ccd584eee618830b77cbf70572e3aebcd6ce
dei20 <107045601+TomCP20@users.noreply.github.com>
2024-12-02 08:59:21 +0000

    solved day 2 with C#

--- 2024/Day 02/C#/Program.cs
+++ 2024/Day 02/C#/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿static bool isSafe(IEnumerable<int> report)
+{
+    int[] differeces = report.Zip(report.Skip(1), (a, b) => a - b).ToArray();
+    return Array.TrueForAll(differeces, x => 1 <= x && x <= 3) || Array.TrueForAll(differeces, x => 1 <= -x && -x <= 3);
+}
+
+List<string> lines = [];
+while (Console.ReadLine() is string line) { lines.Add(line); }
+int[][] reports = lines.Select(x => x.Split(" ").Select(x => int.Parse(x)).ToArray()).ToArray();
+
+Console.WriteLine(reports.Count(isSafe));
+
+Console.WriteLine(reports.Count(report => isSafe(report) || Enumerable.Range(0, report.Length).Any(i => isSafe(report.Where((_, ri) => ri != i)))));
