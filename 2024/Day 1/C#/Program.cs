e748371e4f3f46f800e19a05adeebced8448f4a4
dei20 <107045601+TomCP20@users.noreply.github.com>
2024-12-02 03:43:00 +0000

    refactored day 1 C#

--- 2024/Day 1/C#/Program.cs
+++ 2024/Day 1/C#/Program.cs
@@ -1,13 +1,10 @@
 ﻿List<string> lines = [];
-while (Console.ReadLine() is string line)
-{
-    lines.Add(line);
-}
-List<List<int>> lists = lines.Select(x => x.Split("   ").Select(x => int.Parse(x)).ToList()).ToList();
-List<int> left = [.. lists.Select(x => x[0]).Order()];
-List<int> right = [.. lists.Select(x => x[1]).Order()];
+while (Console.ReadLine() is string line) { lines.Add(line); }
+int[][] lists = lines.Select(x => x.Split("   ").Select(x => int.Parse(x)).ToArray()).ToArray();
+int[] left = [.. lists.Select(x => x[0]).Order()];
+int[] right = [.. lists.Select(x => x[1]).Order()];
 
 Console.WriteLine(left.Zip(right, (l, r) => Math.Abs(l - r)).Sum());
+
 Dictionary<int, int> count = right.GroupBy(x => x).ToDictionary(g => g.Key, g => g.Count());
-int sum = left.Where(count.ContainsKey).Select(x => x*count[x]).Sum();
-Console.WriteLine(sum);
+Console.WriteLine(left.Where(count.ContainsKey).Select(x => x*count[x]).Sum());
