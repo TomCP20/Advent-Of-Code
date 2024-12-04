5c11bb0c5ef7ffc9a1aad2043336e633b8fd6cc4
dei20 <107045601+TomCP20@users.noreply.github.com>
2024-12-04 22:10:57 +0000

    solved day 3 in rust

--- 2024/Day 03/Rust/src/main.rs
+++ 2024/Day 03/Rust/src/main.rs
@@ -1,3 +1,36 @@
+use regex::{Match, Regex};
+use std::io;
+
+fn int_parse(str: Option<Match>) -> i32 {
+    return str.unwrap().as_str().parse::<i32>().unwrap();
+}
+
 fn main() {
-    println!("Hello, world!");
+    let input = io::stdin()
+        .lines()
+        .collect::<Result<Vec<String>, io::Error>>()
+        .unwrap()
+        .join("");
+
+    let patttern1 = Regex::new(r"mul\((\d+),(\d+)\)").unwrap();
+    let sum1: i32 = patttern1
+        .captures_iter(&input)
+        .map(|m| int_parse(m.get(1)) * int_parse(m.get(2)))
+        .sum();
+    println!("{:?}", sum1);
+
+    let patttern2 = Regex::new(r"do\(\)|don\'t\(\)|mul\((\d+),(\d+)\)").unwrap();
+    let mut enabled = true;
+    let mut sum2 = 0;
+    for m in patttern2.captures_iter(&input) {
+        let s = m.get(0).unwrap().as_str();
+        if s == "do()" {
+            enabled = true;
+        } else if s == "don't()" {
+            enabled = false;
+        } else if enabled {
+            sum2 += int_parse(m.get(1)) * int_parse(m.get(2));
+        }
+    }
+    println!("{:?}", sum2);
 }
