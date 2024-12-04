4b5cfaefa3806d70ec727354d32da3b693d745db
dei20 <107045601+TomCP20@users.noreply.github.com>
2024-12-04 21:40:24 +0000

    completed day 3 in F#

--- 2024/Day 03/F#/Program.fs
+++ 2024/Day 03/F#/Program.fs
@@ -1,2 +1,24 @@
-﻿// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+﻿open System
+open System.Text.RegularExpressions
+
+let read _ = Console.ReadLine()
+let isValid = function null -> false | _ -> true
+let input = Seq.initInfinite read |> Seq.takeWhile isValid |> String.concat ""
+
+let pattern1 = Regex(@"mul\((\d+),(\d+)\)")
+printfn "%A" (pattern1.Matches(input) |> Seq.map (fun m -> int(m.Groups[1].ToString()) * int(m.Groups[2].ToString())) |> Seq.sum)
+
+let pattern2 = Regex(@"do\(\)|don\'t\(\)|mul\((\d+),(\d+)\)")
+let rec calc (matches: Match list) (total: int) (enabled: bool)  =
+    match matches with
+    | [] -> total
+    | head :: tail -> 
+        match head.Groups[0].ToString() with
+        | "do()" -> calc tail total true
+        | "don't()" -> calc tail total false
+        | _ -> 
+            match enabled with
+            | false -> calc tail total enabled
+            | true -> calc tail (total+int(head.Groups[1].ToString()) * int(head.Groups[2].ToString())) enabled
+
+printfn "%A" (calc (pattern2.Matches(input) |> Seq.toList) 0 true)
