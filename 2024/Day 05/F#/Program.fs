dfa53069a5d3e67ad3745fdbea8a3822e24135ca
dei20 <107045601+TomCP20@users.noreply.github.com>
2024-12-05 20:10:01 +0000

    completed day 5 in F#

--- 2024/Day 05/F#/Program.fs
+++ 2024/Day 05/F#/Program.fs
@@ -1,2 +1,29 @@
-﻿// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+﻿open System
+let read _ = Console.ReadLine()
+let isValid = function null -> false | _ -> true
+let input: string array = Seq.initInfinite read |> Seq.takeWhile isValid |> Seq.toArray |> String.concat "\n" |> fun f -> f.Split("\n\n", 2)
+let rules: (int * int) list = input[0].Split("\n") |> Array.map(fun rule -> rule.Split("|", 2) |> Array.map(int) |> fun r -> (r[0], r[1])) |> Array.toList
+let updates: int list list = input[1].Split("\n") |> Array.map(fun rule -> rule.Split(",") |> Array.map(int) |> Array.toList) |> Array.toList
+
+let isOrdered (update: int list) (rules: (int * int) list) = rules |> Seq.map(fun (l, r) -> not ((update |> List.contains l) && (update |> List.contains r) && not ((update |> List.findIndex (fun u -> u = l)) < (update |> List.findIndex (fun u -> u = r))))) |> Seq.forall id
+
+let rec searchRules (rules: (int * int) list) (a: int) (b: int) =
+    match rules with
+    | [] -> 0
+    | (r, l)::tail -> 
+        if (a = l && b = r) then 1
+        elif (b = l && a = r) then -1
+        else searchRules tail a b
+
+let rec sumMiddle (updates: int list list) (rules: (int * int) list) (sum1: int) (sum2: int) =
+    match updates with
+    | [] -> (sum1, sum2)
+    | head :: tail -> 
+        let mid = (head.Length - 1)/2
+        match isOrdered head rules with
+        | true -> sumMiddle tail rules (sum1+head[mid]) sum2
+        | false -> 
+            let sotedHead: list<int> = List.sortWith (searchRules rules) head
+            sumMiddle tail rules  sum1 (sum2+sotedHead[mid])
+
+printfn "%A" (sumMiddle updates rules 0 0)
