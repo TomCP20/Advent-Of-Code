6b035ca4a5f5d393db6ef08ee9c72b08782e0a31
dei20 <107045601+TomCP20@users.noreply.github.com>
2024-12-05 02:34:09 +0000

    solved day 4 in C#

--- 2024/Day 04/C#/Program.cs
+++ 2024/Day 04/C#/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Linq;
+using System.Text.RegularExpressions;
+
+List<string> lines = [];
+while (Console.ReadLine() is string line) { lines.Add(line); }
+int row = lines[0].Length + 6;
+string input = string.Join("", lines.Select(line => "..." + line + "...")) ;
+int[] offsets = [0, row, row-1, row-2];
+Console.WriteLine(offsets.Select(o => new Regex(@"(?=(X.{" + o + @"}M.{" + o + @"}A.{" + o + @"}S|S.{" + o + @"}A.{" + o + @"}M.{" + o + @"}X))").Matches(input).Count).Sum());
+string[] orientation = ["MMSS", "MSMS", "SMSM", "SSMM"];
+int n = row-2;
+Console.WriteLine(orientation.Select(o => new Regex(@"(?=(" + o[0] + @"." + o[1] + @".{" + n + @"}A.{" + n + @"}" + o[2] + @"." + o[3] + @"))").Matches(input).Count).Sum());
