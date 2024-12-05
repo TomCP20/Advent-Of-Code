1fb1e19ffaefa23420490f50f6bf20e8dafa7830
dei20 <107045601+TomCP20@users.noreply.github.com>
2024-12-05 16:08:36 +0000

    refactored python day 5

--- 2024/Day 05/Python/main.py
+++ 2024/Day 05/Python/main.py
@@ -1,3 +1,15 @@
+from functools import cmp_to_key
+
+def getCmp(rules: list[list[int]]):
+    def cmp(a: int, b: int):
+        for (l, r) in rules:
+            if a == l and b == r:
+                return 1
+            if b == l and a == r:
+                return -1
+        return 0
+    return cmp
+
 def isSorted(rules: list[list[int]], update: list[int]) -> bool:
     return all(not (l in update and r in update and not (update.index(l) < update.index(r))) for (l,r) in rules)
 
@@ -14,13 +26,7 @@ for update in updates:
     if isSorted(rules, update):
         sum1 += update[mid]
     else:
-        while(not isSorted(rules, update)):
-            for (l, r) in rules:
-                if (l in update and r in update):
-                    li = update.index(l)
-                    ri = update.index(r)
-                    if (not (li < ri)):
-                        update[li], update[ri] = update[ri], update[li]
+        update.sort(key=cmp_to_key(getCmp(rules)))
         sum2 += update[mid]
 print(sum1)
 print(sum2)
