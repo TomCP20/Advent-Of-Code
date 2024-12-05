45dc20e0e113e316479419d1c530a30a71d13215
dei20 <107045601+TomCP20@users.noreply.github.com>
2024-12-05 07:21:07 +0000

    refactored day 5 python code

--- 2024/Day 05/Python/main.py
+++ 2024/Day 05/Python/main.py
@@ -1,11 +1,5 @@
 def isSorted(rules: list[list[int]], update: list[int]) -> bool:
-    for rule in rules:
-        l, r = rule
-        if (not (l in update and r in update)):
-            continue
-        if (not (update.index(l) < update.index(r))):
-            return False
-    return True
+    return all(not (l in update and r in update and not (update.index(l) < update.index(r))) for (l,r) in rules)
 
 input = open(0).read()
 a, b = input.split("\n\n", 1)
@@ -16,20 +10,17 @@ updates = [list(map(int, update.split(","))) for update in b.splitlines()]
 sum1 = 0
 sum2 = 0
 for update in updates:
-    passed = isSorted(rules, update)
     mid = int((len(update) - 1)/2)
-    if passed:
+    if isSorted(rules, update):
         sum1 += update[mid]
     else:
         while(not isSorted(rules, update)):
-            for rule in rules:
-                l, r = rule
-                if (not (l in update and r in update)):
-                    continue
-                li = update.index(l)
-                ri = update.index(r)
-                if (not (li < ri)):
-                    update[li], update[ri] = update[ri], update[li]
+            for (l, r) in rules:
+                if (l in update and r in update):
+                    li = update.index(l)
+                    ri = update.index(r)
+                    if (not (li < ri)):
+                        update[li], update[ri] = update[ri], update[li]
         sum2 += update[mid]
 print(sum1)
 print(sum2)
