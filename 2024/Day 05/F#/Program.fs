4f0a51c8d9238363c05acb41f3fe4e52397602f5
dei20 <107045601+TomCP20@users.noreply.github.com>
2024-12-06 03:39:11 +0000

    renamed searchRules  to compare

--- 2024/Day 05/F#/Program.fs
+++ 2024/Day 05/F#/Program.fs
@@ -7,13 +7,13 @@ let updates: int list list = input[1].Split("\n") |> Array.map(fun rule -> rule.
 
 let isOrdered (update: int list) (rules: (int * int) list) = rules |> Seq.map(fun (l, r) -> not ((update |> List.contains l) && (update |> List.contains r) && not ((update |> List.findIndex (fun u -> u = l)) < (update |> List.findIndex (fun u -> u = r))))) |> Seq.forall id
 
-let rec searchRules (rules: (int * int) list) (a: int) (b: int) =
+let rec compare (rules: (int * int) list) (a: int) (b: int) =
     match rules with
     | [] -> 0
     | (r, l)::tail -> 
         if (a = l && b = r) then 1
         elif (b = l && a = r) then -1
-        else searchRules tail a b
+        else compare tail a b
 
 let rec sumMiddle (updates: int list list) (rules: (int * int) list) (sum1: int) (sum2: int) =
     match updates with
@@ -23,7 +23,7 @@ let rec sumMiddle (updates: int list list) (rules: (int * int) list) (sum1: int)
         match isOrdered head rules with
         | true -> sumMiddle tail rules (sum1+head[mid]) sum2
         | false -> 
-            let sotedHead: list<int> = List.sortWith (searchRules rules) head
+            let sotedHead: list<int> = List.sortWith (compare rules) head
             sumMiddle tail rules  sum1 (sum2+sotedHead[mid])
 
 printfn "%A" (sumMiddle updates rules 0 0)
