bd870f6e30b25f4684278106d9a68f19de9fe7a1
dei20 <107045601+TomCP20@users.noreply.github.com>
2024-12-11 20:12:33 +0000

    refactored python day 7

--- 2024/Day 07/Python/main.py
+++ 2024/Day 07/Python/main.py
@@ -1,20 +1,22 @@
+import math
+
 def parseline(line: str) -> tuple[int, list[int]]:
     a, b = line.split(": ")
     val = int(a)
     nums = list(map(int, b.split(" ")))
     return val, nums
 
-def check(val: int, nums: list[int]):
+def check(val: int, nums: list[int], part2: bool):
     if len(nums) == 1:
         return nums[0] == val
-    if check(val, [nums[0] + nums[1]] + nums[2:]):
+    if check(val, [nums[0] + nums[1]] + nums[2:], part2):
         return True
-    if check(val, [nums[0] * nums[1]] + nums[2:]):
+    if check(val, [nums[0] * nums[1]] + nums[2:], part2):
         return True
-    if check(val, [int(str(nums[0]) + str(nums[1]))] + nums[2:]):
+    if part2 and check(val, [(nums[0] * (10 ** (int(math.log10(nums[1])) + 1))) + nums[1]] + nums[2:], part2):
         return True
     return False
 
 input = list(map(parseline, open(0).read().splitlines()))
-total = sum(val for val, nums in input if check(val, nums))
-print(total)
+print(sum(val for val, nums in input if check(val, nums, False)))
+print(sum(val for val, nums in input if check(val, nums, True)))
