bd466f5f21be5616fd051459f3001ad02d46aef8
dei20 <107045601+TomCP20@users.noreply.github.com>
2024-12-17 22:36:12 +0000

    solved day 16 part 2

--- 2024/Day 16/Python/main.py
+++ 2024/Day 16/Python/main.py
@@ -1,31 +1,44 @@
+from heapq import heappop, heappush
+
+
 type Vec = tuple[int, int]
 type State = tuple[Vec, int]
 
 def add(a: Vec, b: Vec) -> Vec:
     return (a[0] + b[0], a[1] + b[1])
 
-def h(n: State, end: Vec):
-    return abs(n[0][0] - end[0]) + abs(n[0][1] - end[1])
+def Dijkstra(start: State, end: Vec, maze: list[str]):
+    Q: list[tuple[int, State]] = []
+    dist = {start: 0}
+    heappush(Q, (0, start))
+    prev: dict[State, set[State]] = dict()
 
-def a_star(start: State, end: Vec, maze: list[str]):
-    openSet = {start}
+    while Q:
+        _, current = heappop(Q)
 
-    gScore = {start: 0}
-
-    fScore = {start: h(start, end)}
-
-    while openSet:
-        current = min(openSet, key=lambda x: fScore.get(x, float("inf")))
-        openSet.remove(current)
         if current[0] == end:
-            return gScore[current]
+            break
+
         for neighbor, d in get_neighbors(current, maze):
-            tentative_gScore = gScore[current] + d
-            if tentative_gScore < gScore.get(neighbor, float("inf")):
-                gScore[neighbor] = tentative_gScore
-                fScore[neighbor] = tentative_gScore + h(neighbor, end)
-                openSet.add(neighbor)
-    return None
+            alt = dist[current] + d
+            if alt < dist.get(neighbor, float("inf")):
+                prev[neighbor] = {current}
+                dist[neighbor] = alt
+                heappush(Q, (alt, neighbor))
+            elif alt == dist.get(neighbor, float("inf")):
+                prev[neighbor].add(current)
+    
+
+    def get_path(state: State):
+        if state[0] == start[0]:
+            yield [state]
+            return
+        for prev_state in prev[state]:
+            for path in get_path(prev_state):
+                yield path + [state]
+    
+    return dist[current], get_path(current)
+
 
 def get_neighbors(n: State, maze: list[str]):
     pos, turns = n
@@ -49,5 +62,7 @@ for y, line in enumerate(maze):
         elif c == "E":
             end = (x, y)
 
-dist = a_star((start, 0), end, maze)
+dist, paths = Dijkstra((start, 0), end, maze)
+
 print(dist)
+print(len({state[0] for path in paths for state in path}))
