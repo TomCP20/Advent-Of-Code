4b7361f6f86b5670f853f1eeff43f90746631e49
dei20 <107045601+TomCP20@users.noreply.github.com>
2024-12-19 01:34:26 +0000

    switched from A* to BFS

--- 2024/Day 18/Python/main.py
+++ 2024/Day 18/Python/main.py
@@ -1,34 +1,30 @@
+from collections import deque
+
 Vec = tuple[int, int]
 VecSet = set[Vec]
 VecList = list[Vec]
-ScoreDict = dict[Vec, int]
+VecQueue = deque[Vec]
+DistDict = dict[Vec, int]
 
-def A_Star(size: int, corrupted: VecSet):
-    def h(n: Vec):
-        return 2*size - n[0] - n[1]
+def BFS(size: int, corrupted: VecSet) -> (int | None):
     def neighbors(n: Vec):
         for dir in [(0,1),(1,0),(0,-1),(-1,0)]:
             new = (n[0] + dir[0], n[1] + dir[1])
             if (new not in corrupted) and 0 <= new[0] <= size and 0 <= new[1] <= size:
                 yield new
     
-    openSet: VecSet = {(0, 0)}
+    q: VecQueue = deque()
+    q.append((0, 0))
+    dist: DistDict = {(0, 0): 0}
 
-    gScore: ScoreDict = {(0, 0): 0}
-    fScore: ScoreDict = {(0, 0): h((0, 0))}
-
-    while openSet:
-        current = min(openSet, key= lambda x: fScore[x])
-        openSet.remove(current)
+    while q:
+        current = q.popleft()
         if current == (size, size):
-            return gScore[current]
+            return dist[current]
         for neighbor in neighbors(current):
-            tentative_gScore = gScore[current] + 1
-            if tentative_gScore < gScore.get(neighbor, float("inf")):
-                gScore[neighbor] = tentative_gScore
-                fScore[neighbor] = tentative_gScore + h(neighbor)
-                if neighbor not in openSet:
-                    openSet.add(neighbor)
+            if neighbor not in dist.keys():
+                dist[neighbor] = dist[current] + 1
+                q.append(neighbor)
     return None
 
 #size = 6
@@ -38,8 +34,8 @@ size = 70
 bytes = 1024
 
 all_corrupted: VecList = [(int(line.split(",", 1)[0]), int(line.split(",", 1)[1])) for line in open(0).read().splitlines()]
-print(A_Star(size, set(all_corrupted[:bytes])))
+print(BFS(size, set(all_corrupted[:bytes])))
 
-while A_Star(size, set(all_corrupted[:bytes])):
+while BFS(size, set(all_corrupted[:bytes])):
     bytes += 1
 print(f"{all_corrupted[bytes-1][0]},{all_corrupted[bytes-1][1]}")
