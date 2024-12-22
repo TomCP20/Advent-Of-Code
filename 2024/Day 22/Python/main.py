b630bec9446676a7143d569cc04fdcdf486264e7
dei20 <107045601+TomCP20@users.noreply.github.com>
2024-12-22 17:49:11 +0000

    solved day 22 part 2

--- 2024/Day 22/Python/main.py
+++ 2024/Day 22/Python/main.py
@@ -1,3 +1,5 @@
+from collections import Counter, deque
+
 def step(n: int) -> int:
     n = ((n*64)^n)%16777216
     n = ((n//32)^n)%16777216
@@ -9,10 +11,27 @@ def step2000(n: int) -> int:
         n = step(n)
     return n
 
-def getPrices(n: int):
-    for _ in range(2000):
-        n = step(n)
-        yield n % 10
 
-initial = map(int, open(0).read().splitlines())
+def sequenceToPrice(n: int) -> Counter[tuple[int, ...]]:
+    slice: deque[int] = deque([], maxlen=4)
+    prices = Counter()
+
+    for i in range(2000):
+        if i >= 4:
+            key = tuple(slice)
+            if key not in prices:
+                prices[key] = n % 10
+
+        next = step(n)
+        slice.append((next%10)-(n%10))
+        n = next
+
+    return prices
+
+initial = list(map(int, open(0).read().splitlines()))
 print(sum(step2000(n) for n in initial))
+
+all_prices = Counter()
+for n in initial:
+    all_prices += sequenceToPrice(n)
+print(all_prices.most_common(1)[0][1])
