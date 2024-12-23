9e2fb1c0ab3d34f3a2417307b96de67f00c8a4de
dei20 <107045601+TomCP20@users.noreply.github.com>
2024-12-23 19:04:17 +0000

    Refactored 2024 day 23 python

--- 2024/Day 23/Python/main.py
+++ 2024/Day 23/Python/main.py
@@ -1,15 +1,10 @@
 from itertools import permutations
+from collections import defaultdict
 
-N: dict[str, set[str]] = {}
+N: dict[str, set[str]] = defaultdict(set)
 for a, b in (line.split("-") for line in open(0).read().splitlines()):
-    if a in N:
-        N[a].add(b)
-    else:
-        N[a] = {b}
-    if b in N:
-        N[b].add(a)
-    else:
-        N[b] = {a}
+    N[a].add(b)
+    N[b].add(a)
 
 print(len({frozenset({a, b, t}) for t in N if t[0] == "t" for a, b in permutations(N[t], 2) if b in N[a]}))
 
@@ -19,6 +14,6 @@ def BronKerbosch(R: set[str], P: set[str], X: set[str]):
     while P:
         v = P.pop()
         yield from BronKerbosch( R | {v}, P & N[v], X & N[v] )
-        X |= {v}
+        X.add(v)
 
 print(",".join(sorted(max(BronKerbosch(set(), set(N), set()), key=len))))
