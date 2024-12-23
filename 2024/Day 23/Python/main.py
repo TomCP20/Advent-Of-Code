a065c9828c6171ff8be8b407b4d7c69a3983620d
dei20 <107045601+TomCP20@users.noreply.github.com>
2024-12-23 23:51:28 +0000

    refactored 2024 day 23 python

--- 2024/Day 23/Python/main.py
+++ 2024/Day 23/Python/main.py
@@ -1,19 +1,17 @@
 from itertools import permutations
 from collections import defaultdict
 
-N: dict[str, set[str]] = defaultdict(set)
-for a, b in (line.split("-") for line in open(0).read().splitlines()):
-    N[a].add(b)
-    N[b].add(a)
-
-print(len({frozenset({a, b, t}) for t in N if t[0] == "t" for a, b in permutations(N[t], 2) if b in N[a]}))
-
-def BronKerbosch(R: set[str], P: set[str], X: set[str]):
+def BronKerbosch(N: dict[str, set[str]], R: set[str], P: set[str], X: set[str]):
     if not P and not X:
         yield R
     while P:
         v = P.pop()
-        yield from BronKerbosch( R | {v}, P & N[v], X & N[v] )
+        yield from BronKerbosch(N, R | {v}, P & N[v], X & N[v])
         X.add(v)
 
-print(",".join(sorted(max(BronKerbosch(set(), set(N), set()), key=len))))
+N: dict[str, set[str]] = defaultdict(set)
+for a, b in (line.split("-") for line in open(0).read().splitlines()):
+    N[a].add(b)
+    N[b].add(a)
+print(len({frozenset({a, b, t}) for t in N if t[0] == "t" for a, b in permutations(N[t], 2) if b in N[a]}))
+print(",".join(sorted(max(BronKerbosch(N, set(), set(N), set()), key=len))))
