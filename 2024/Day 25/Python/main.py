119ad8bbc2890dae687a417286bcff1014923a9d
dei20 <107045601+TomCP20@users.noreply.github.com>
2024-12-25 14:00:42 +0000

    refactored day 25 python

--- 2024/Day 25/Python/main.py
+++ 2024/Day 25/Python/main.py
@@ -1,11 +1,7 @@
 from itertools import product
 
 schematics = list(map(lambda x : x.splitlines(), open(0).read().split("\n\n")))
-w = 5
-h = 7
-locks = [schematic for schematic in schematics if schematic[0] == "#"*w]
-keys = [schematic for schematic in schematics if schematic[0] == "."*w]
-locks = [[[line[x] for line in lock].count("#")-1 for x in range(w)] for lock in locks]
-keys = [[[line[x] for line in key].count("#")-1 for x in range(w)] for key in keys]
+locks = [[[line[x] for line in lock].count("#") for x in range(5)] for lock in schematics if lock[0] == "#####"]
+keys = [[[line[x] for line in key].count("#") for x in range(5)] for key in schematics if key[0] == "....."]
 
-print(sum(all(((k + l) <= h-2) for (k, l) in zip(key, lock)) for key, lock in product(keys, locks)))
+print(sum(all(((k + l) <= 7) for (k, l) in zip(key, lock)) for key, lock in product(keys, locks)))
