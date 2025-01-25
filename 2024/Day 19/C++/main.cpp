322106fb7272ec7cfe100d9747378888335049dd
dei20 <107045601+TomCP20@users.noreply.github.com>
2025-01-25 16:27:44 +0000

    solved 2024 day 19 part 2 in C++

--- 2024/Day 19/C++/main.cpp
+++ 2024/Day 19/C++/main.cpp
@@ -22,9 +22,74 @@ std::vector<std::string> generate_possible_designs(const std::vector<std::string
         {
             possible_designs.push_back(d);
         }
-        
     }
     return possible_designs;
+}
+
+const std::string cols = "wubrg";
+const int cols_num = 5;
+
+struct Trie
+{
+    Trie *children[cols_num];
+    bool end;
+    Trie()
+    {
+        end = false;
+        for (int i = 0; i < cols_num; i++)
+        {
+            children[i] = nullptr;
+        }
+    }
+};
+
+void insert_towl(Trie *root, const std::string &towl)
+{
+    Trie *ptr = root;
+    for (char c : towl)
+    {
+        int index = cols.find(c);
+        if (ptr->children[index] == nullptr)
+        {
+            Trie *new_node = new Trie();
+            ptr->children[index] = new_node;
+        }
+
+        ptr = ptr->children[index];
+    }
+    ptr->end = true;
+}
+
+unsigned long long ways_of_forming_design(Trie *root, const std::string &design)
+{
+    int n = design.size();
+    std::vector<unsigned long long> count(n, 0);
+
+    for (int i = 0; i < n; i++)
+    {
+        Trie *ptr = root;
+        for (int j = i; j >= 0; j--)
+        {
+            int index = cols.find(design[j]);
+            if (ptr->children[index] == nullptr)
+            {
+                break;
+            }
+            ptr = ptr->children[index];
+            if (ptr->end)
+            {
+                if (j > 0)
+                {
+                    count[i] += count[j - 1];
+                }
+                else
+                {
+                    count[i] += 1;
+                }
+            }
+        }
+    }
+    return count[n - 1];
 }
 
 int main(int argc, char *argv[])
@@ -56,5 +121,19 @@ int main(int argc, char *argv[])
             }
         }
     }
-    std::cout << generate_possible_designs(towls, designs).size() << "\n";
+    std::vector<std::string> possible_designs = generate_possible_designs(towls, designs);
+    std::cout << possible_designs.size() << "\n";
+
+    Trie *root = new Trie();
+    for (std::string t : towls)
+    {
+        std::reverse(t.begin(), t.end());
+        insert_towl(root, t);
+    }
+    unsigned long long s = 0;
+    for (auto &&d : possible_designs)
+    {
+        s += ways_of_forming_design(root, d);
+    }
+    std::cout << s << "\n";
 }
