c356a09af2e22e0dfe81918eb2b33c4b7961d61e
dei20 <107045601+TomCP20@users.noreply.github.com>
2025-03-14 17:05:46 +0000

    refactor 2024 day 25 Python

--- 2024/Day 25/Python/main.py
+++ 2024/Day 25/Python/main.py
@@ -1,8 +1,18 @@
 """Advent of Code - 2024 - Day 25"""
-from itertools import product
-with open(0, encoding="utf-8") as f:
-    schematics = list(map(lambda x : x.splitlines(), f.read().split("\n\n")))
-locks = [[[r[x] for r in l].count("#") for x in range(5)] for l in schematics if l[0] == "#####"]
-keys = [[[r[x] for r in k].count("#") for x in range(5)] for k in schematics if k[0] == "....."]
 
-print(sum(all(((k + l) <= 7) for (k, l) in zip(key, lock)) for key, lock in product(keys, locks)))
+from itertools import product
+
+with open(0, encoding="utf-8") as f:
+    schematics = [x.splitlines() for x in f.read().split("\n\n")]
+
+str_locks = filter(lambda s: s[0] == "#####", schematics)
+str_keys = filter(lambda s: s[0] == ".....", schematics)
+
+locks = [[col.count("#") for col in zip(*l)] for l in str_locks]
+keys = [[col.count("#") for col in zip(*k)] for k in str_keys]
+
+print(
+    sum(
+        all(k + l <= 7 for k, l in zip(key, lock)) for key, lock in product(keys, locks)
+    )
+)
