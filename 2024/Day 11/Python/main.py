9ca30581cf6bc36f3eedadeb93f591fc8dfebcac
dei20 <107045601+TomCP20@users.noreply.github.com>
2025-03-23 15:32:37 +0000

    refactor 2024 day 11 python

--- 2024/Day 11/Python/main.py
+++ 2024/Day 11/Python/main.py
@@ -1,6 +1,8 @@
 """Advent of Code - 2024 - Day 11"""
+
 from functools import cache
 import math
+
 
 @cache
 def process_stone(stone: int):
@@ -9,20 +11,24 @@ def process_stone(stone: int):
         return 1
     digits = int(math.log10(stone)) + 1
     if digits % 2 == 0:
-        a: int = stone // 10 ** ((digits//2))
-        b: int = stone % (10**(digits//2))
+        a: int = stone // 10 ** ((digits // 2))
+        b: int = stone % (10 ** (digits // 2))
         return (a, b)
     return stone * 2024
+
 
 @cache
 def num_stones(stone: int, blinks: int, max_blinks: int) -> int:
     """calculates the number of stones a stone creates after blinks number of blinks"""
     if blinks == max_blinks:
         return 1
-    x = process_stone(stone)
-    if isinstance(x, tuple):
-        return num_stones(x[0], blinks + 1, max_blinks) + num_stones(x[1], blinks + 1, max_blinks)
-    return num_stones(x, blinks + 1, max_blinks)
+    blinks += 1
+    match process_stone(stone):
+        case (a, b):
+            return num_stones(a, blinks, max_blinks) + num_stones(b, blinks, max_blinks)
+        case stone:
+            return num_stones(stone, blinks, max_blinks)
+
 
 with open(0, encoding="utf-8") as f:
     stones = list(map(int, f.read().split()))
