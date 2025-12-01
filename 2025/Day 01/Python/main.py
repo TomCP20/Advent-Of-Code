5a32ae5c827ecd570dd51443e2f66b4a1bbc5637
dei20 <107045601+TomCP20@users.noreply.github.com>
2025-12-01 17:58:15 +0000

    refactored Day 1

--- 2025/Day 01/Python/main.py
+++ 2025/Day 01/Python/main.py
@@ -1,43 +1,36 @@
 """Advent of Code - 2025 - Day 1"""
 
-with open(0, encoding="utf-8") as f:
-    lines = f.read().splitlines()
+def parse_line(line: str) -> tuple[int, int]:
+    """Parses a line of the input"""
+    direction = 1 if line[0] == "R" else -1
+    return (direction, int(line[1:]))
 
-def part_1(rotations: list[str]) -> int:
-    """Function that solves Part 1 of Day 1"""
+def part_1(rotations: list[tuple[int, int]]) -> int:
+    """Solves Part 1 of Day 1"""
     dial = 50
     password = 0
-
-    for rotation in rotations:
-        direction = rotation[:1]
-        distance = int(rotation[1:])
-        if direction == "R":
-            dial = (dial + distance) % 100
-        elif direction == "L":
-            dial = (dial - distance) % 100
+    for direction, distance in rotations:
+        dial = (dial + direction*distance) % 100
         if dial == 0:
             password += 1
     return password
 
-def part_2(rotations: list[str]) -> int:
-    """Function that solves Part 2 of Day 1"""
+def part_2(rotations: list[tuple[int, int]]) -> int:
+    """Solves Part 2 of Day 1"""
     dial = 50
     password = 0
-
-    for rotation in rotations:
-        direction = rotation[:1]
-        distance = int(rotation[1:])
-        if direction == "R":
-            for _ in range(distance):
-                dial = (dial + 1) % 100
-                if dial == 0:
-                    password += 1
-        elif direction == "L":
-            for _ in range(distance):
-                dial = (dial - 1) % 100
-                if dial == 0:
-                    password += 1
+    for direction, distance in rotations:
+        for _ in range(distance):
+            dial = (dial + direction) % 100
+            if dial == 0:
+                password += 1
     return password
 
-print(part_1(lines))
-print(part_2(lines))
+def main():
+    """Solves Part 1 and 2 of Day 1"""
+    with open(0, encoding="utf-8") as f:
+        rotations = [parse_line(line) for line in f.read().splitlines()]
+    print(part_1(rotations))
+    print(part_2(rotations))
+
+main()
