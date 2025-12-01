11dc705a2101d94aa86009bc160c81bed3961280
dei20 <107045601+TomCP20@users.noreply.github.com>
2025-12-01 00:49:55 +0000

    Updated setup.py

--- setup.py
+++ setup.py
@@ -5,21 +5,22 @@ import subprocess
 
 dirname = os.path.dirname(__file__)
 
-YEAR = "2024"
+YEAR = 2025
+DAYS = 12
 INPUTS = True
 PYTHON = True
-CPP = True
+CPP = False
 CS = True
-FS = True
-RUST = True
+FS = False
+RUST = False
 
 def makefile(directory: str, name: str):
     """creates an empty file with name name in directory directory"""
     with open(os.path.join(directory, name), 'x', encoding="utf-8"):
         pass
 
-for i in range(1, 26):
-    day_path = os.path.join(dirname, YEAR, f"Day {i:02d}")
+for i in range(1, DAYS+1):
+    day_path = os.path.join(dirname, str(YEAR), f"Day {i:02d}")
     if INPUTS:
         makefile(day_path, "test.txt")
         makefile(day_path, "input.txt")
