170aa2ce87a70c4ce7e3e3d303328374d15690ce
dei20 <107045601+TomCP20@users.noreply.github.com>
2025-12-02 12:42:27 +0000

    refactored part 1 of day 2

--- 2025/Day 02/Python/main.py
+++ 2025/Day 02/Python/main.py
@@ -1,22 +1,25 @@
 """Advent of Code - 2025 - Day 2"""
 
-def getRange(r: str):
+def get_range(r: str):
+    """create a range based on the string r"""
     start = int(r[:r.find("-")])
     end = int(r[r.find("-")+1:])
     return range(start, end+1)
 
-def isInvalid(ID: str) -> bool:
+def is_invalid1(ID: str) -> bool:
+    """Checks if the id is valid based on part 1 rules"""
     return ID[:len(ID)//2] == ID[len(ID)//2:]
 
-def main():
+def part_1(ids: list[int]):
+    """Solves Part 1 of Day 1"""
+    return sum(int(id) for id in ids if is_invalid1(str(id)))
 
+def main():
+    """Solves Part 1 of Day 1"""
     with open(0, encoding="utf-8") as f:
         line = "".join(f.read().splitlines())
-
-    ranges = (getRange(r) for r in line.split(","))
-
+    ranges = (get_range(r) for r in line.split(","))
     ids = [id for r in ranges for id in r]
-    res = sum(int(id) for id in ids if isInvalid(str(id)))
-    print(res)
+    print(part_1(ids))
 
 main()
