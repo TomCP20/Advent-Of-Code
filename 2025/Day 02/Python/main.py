bc96482d9ca807ef0c39cddd9981de8a25155176
dei20 <107045601+TomCP20@users.noreply.github.com>
2025-12-02 12:43:22 +0000

    renamed variables for clarity

--- 2025/Day 02/Python/main.py
+++ 2025/Day 02/Python/main.py
@@ -6,9 +6,9 @@ def get_range(r: str):
     end = int(r[r.find("-")+1:])
     return range(start, end+1)
 
-def is_invalid1(ID: str) -> bool:
+def is_invalid1(id_str: str) -> bool:
     """Checks if the id is valid based on part 1 rules"""
-    return ID[:len(ID)//2] == ID[len(ID)//2:]
+    return id_str[:len(id_str)//2] == id_str[len(id_str)//2:]
 
 def part_1(ids: list[int]):
     """Solves Part 1 of Day 1"""
@@ -19,7 +19,7 @@ def main():
     with open(0, encoding="utf-8") as f:
         line = "".join(f.read().splitlines())
     ranges = (get_range(r) for r in line.split(","))
-    ids = [id for r in ranges for id in r]
+    ids = [id_int for r in ranges for id_int in r]
     print(part_1(ids))
 
 main()
