9b20b120b72c8575e32fbd411b8f62cdcace0d0d
dei20 <107045601+TomCP20@users.noreply.github.com>
2025-12-03 02:42:32 +0000

    solved day 2 in C#

--- 2025/Day 02/C#/Program.cs
+++ 2025/Day 02/C#/Program.cs
@@ -1,2 +1,52 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Text.RegularExpressions;
+
+static partial class Program
+{
+    [GeneratedRegex("^(.+)\\1$")]
+    private static partial Regex Regex1();
+    [GeneratedRegex("^(.+)\\1+$")]
+    private static partial Regex Regex2();
+
+    static IEnumerable<long> LongRange(long start, long end)
+    {
+        for (long i = start; i <= end; i++)
+        {
+            yield return i;
+        }
+    }
+
+    static long solve(List<long> ids, int part)
+    {
+        Regex pattern;
+        if (part == 1)
+        {
+            pattern = Regex1();
+        }
+        else
+        {
+            pattern = Regex2();
+        }
+
+        return ids.Where(id => pattern.IsMatch(id.ToString())).Sum();
+    }
+
+    static void Main(string[] args)
+    {
+        string line = "";
+        while (Console.ReadLine() is string l)
+        {
+            line += l;
+        }
+        List<(long start, long end)> ranges = [.. line.Split(",").Select(r =>
+            {
+                int i = r.IndexOf('-');
+                return (long.Parse(r[..i]), long.Parse(r[(i + 1)..]));
+            })];
+
+        List<long> ids = [.. ranges.Select((r) => LongRange(r.start, r.end)).SelectMany(x => x)];
+
+        Console.WriteLine(solve(ids, 1));
+        Console.WriteLine(solve(ids, 2));
+    }
+
+}
