0c5acf5d2444b10304dd70a96b10d5c07dea66f6
dei20 <107045601+TomCP20@users.noreply.github.com>
2025-12-03 23:46:02 +0000

    solved day 3 in C#

--- 2025/Day 03/C#/Program.cs
+++ 2025/Day 03/C#/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+static long maxJoltage(List<int> bank, int digits)
+{
+    if (digits == 1)
+    {
+        return bank.Max();
+    }
+    digits--;
+    int l = bank.Count;
+    int digit = bank.Take(l - digits).Max();
+    return digit * (long)Math.Pow(10, digits) + maxJoltage([.. bank.TakeLast(l - bank.IndexOf(digit) - 1)], digits);
+}
+
+List<List<int>> banks = [];
+while (Console.ReadLine() is string line)
+{
+    List<int> bank = [.. line.Select(c => int.Parse(c.ToString()))];
+    banks.Add(bank);
+}
+
+Console.WriteLine(banks.Select(bank => maxJoltage(bank, 2)).Sum());
+Console.WriteLine(banks.Select(bank => maxJoltage(bank, 12)).Sum());
