9bd36cb1ea0e88e6cf4be08986d95245e8403db7
dei20 <107045601+TomCP20@users.noreply.github.com>
2025-12-06 14:07:56 +0000

    refactored python day 6

--- 2025/Day 06/Python/main.py
+++ 2025/Day 06/Python/main.py
@@ -1,37 +1,40 @@
 """Advent of Code - 2025 - Day 6"""
 
-import math
-import itertools
+from math import prod
+from itertools import groupby
+from typing import Iterable
 
 
-def solve_problems(num_columns: list[list[int]], operators: list[str]):
+def solve_problems(num_columns: Iterable[Iterable[int]], operators: list[str]):
     """Solves Cephalopod math"""
     return sum(
-        sum(nums) if operator == "+" else math.prod(nums)
-        for operator, nums in zip(operators, num_columns)
+        sum(nums) if op == "+" else prod(nums)
+        for op, nums in zip(operators, num_columns)
     )
 
 
-def part_1(lines: list[str]) -> int:
+def part_1(num_lines: list[str], operators: list[str]) -> int:
     """Solves Part 1"""
-    num_rows = map((lambda line: map(int, line.split())), lines[:-1])
-    num_columns = list(map(list, zip(*num_rows)))
-    operators = lines[-1].split()
+    num_rows = map((lambda line: map(int, line.split())), num_lines)
+    num_columns = zip(*num_rows)
     return solve_problems(num_columns, operators)
 
 
-def part_2(lines: list[str]) -> int:
+def part_2(num_lines: list[str], operators: list[str]) -> int:
     """Solves Part 2"""
-    operators = lines[-1].split()
-    str_columns = list(map("".join, zip(*lines[:-1])))
-    groups = itertools.groupby(str_columns, lambda s: s == " " * (len(lines) - 1))
-    num_columns = [list(map(int, group)) for is_gap, group in groups if not is_gap]
+    str_columns = map("".join, zip(*num_lines))
+    groups = groupby(str_columns, lambda s: s == " " * len(num_lines))
+    num_columns = (map(int, group) for is_gap, group in groups if not is_gap)
     return solve_problems(num_columns, operators)
 
 
 def main():
     """Solves Part 1 and 2"""
     with open(0, encoding="utf-8") as f:
-        lines = f.read().splitlines()
-    print(part_1(lines))
-    print(part_2(lines))
+        *num_lines, operator_line = f.read().splitlines()
+    operators = operator_line.split()
+    print(part_1(num_lines, operators))
+    print(part_2(num_lines, operators))
+
+
+main()
