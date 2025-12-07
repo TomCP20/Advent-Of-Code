17c1ddaabcff9f9968252d164dcd9bc8dc1e012e
dei20 <107045601+TomCP20@users.noreply.github.com>
2025-12-07 14:45:02 +0000

    refactored day 7 part 1

--- 2025/Day 07/Python/main.py
+++ 2025/Day 07/Python/main.py
@@ -3,10 +3,10 @@
 
 def part_1(head: str, tail: list[str]):
     beams: set[int] = {head.find("S")}
-    next_beams: set[int] = set()
     solution: int = 0
 
     for line in tail:
+        next_beams: set[int] = set()
         for beam in beams:
             if line[beam] == "^":
                 solution += 1
@@ -15,7 +15,6 @@ def part_1(head: str, tail: list[str]):
             else:
                 next_beams.add(beam)
         beams = next_beams
-        next_beams = set()
     return solution
 
 with open(0, encoding="utf-8") as f:
