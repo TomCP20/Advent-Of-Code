bc510d59983380454e46a74108dd37f69895b603
dei20 <107045601+TomCP20@users.noreply.github.com>
2025-12-07 20:09:38 +0000

    python day 7 refactor

--- 2025/Day 07/Python/main.py
+++ 2025/Day 07/Python/main.py
@@ -7,14 +7,13 @@ beams[head.find("S")] = 1
 splits: int = 0
 for line in tail:
     next_beams: list[int] = [0] * len(head)
-    for i, val in enumerate(beams):
-        if val:
-            if line[i] == "^":
-                splits += 1
-                next_beams[i - 1] += val
-                next_beams[i + 1] += val
-            else:
-                next_beams[i] += val
+    for i, val in filter(lambda x: x[1], enumerate(beams)):
+        if line[i] == "^":
+            splits += 1
+            next_beams[i - 1] += val
+            next_beams[i + 1] += val
+        else:
+            next_beams[i] += val
     beams = next_beams
 print(splits)
 print(sum(beams))
