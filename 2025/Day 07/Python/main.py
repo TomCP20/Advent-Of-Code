7e48e9c2734cb23ee86bd9d65431ee9818246299
dei20 <107045601+TomCP20@users.noreply.github.com>
2025-12-07 19:24:29 +0000

    converted dict to list

--- 2025/Day 07/Python/main.py
+++ 2025/Day 07/Python/main.py
@@ -1,23 +1,20 @@
 """Advent of Code - 2025 - Day 7"""
 
-from collections import defaultdict
-
-
 with open(0, encoding="utf-8") as f:
     head, *tail = f.read().splitlines()
-
-
-beams: dict[int, int] = {head.find("S"): 1}
+beams: list[int] = [0] * len(head)
+beams[head.find("S")] = 1
 splits: int = 0
 for line in tail:
-    next_beams: dict[int, int] = defaultdict(int)
-    for i, val in beams.items():
-        if line[i] == "^":
-            splits += 1
-            next_beams[i - 1] += val
-            next_beams[i + 1] += val
-        else:
-            next_beams[i] += val
+    next_beams: list[int] = [0] * len(head)
+    for i, val in enumerate(beams):
+        if val:
+            if line[i] == "^":
+                splits += 1
+                next_beams[i - 1] += val
+                next_beams[i + 1] += val
+            else:
+                next_beams[i] += val
     beams = next_beams
 print(splits)
-print(sum(beams.values()))
+print(sum(beams))
