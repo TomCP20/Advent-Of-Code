2d2f4ac8ac3c57d3968bdb70f5d1589d493f6fee
dei20 <107045601+TomCP20@users.noreply.github.com>
2025-12-07 14:49:49 +0000

    solved day 7 part 2 in python

--- 2025/Day 07/Python/main.py
+++ 2025/Day 07/Python/main.py
@@ -1,6 +1,9 @@
 """Advent of Code - 2025 - Day 7"""
 
 
+from collections import defaultdict
+
+
 def part_1(head: str, tail: list[str]):
     beams: set[int] = {head.find("S")}
     solution: int = 0
@@ -17,8 +20,28 @@ def part_1(head: str, tail: list[str]):
         beams = next_beams
     return solution
 
+
+
+def part_2(head: str, tail: list[str]):
+    beams: dict[int, int] = {head.find("S"): 1}
+    for line in tail:
+        next_beams: dict[int, int] = defaultdict(int)
+        for i, val in beams.items():
+            if line[i] == "^":
+                next_beams[i-1] += val
+                next_beams[i+1] += val
+            else:
+                next_beams[i] += val
+        beams = next_beams
+    return sum(beams.values())
+
+
+
+
 with open(0, encoding="utf-8") as f:
     head, *tail = f.read().splitlines()
 
 
 print(part_1(head, tail))
+print(part_2(head, tail))
+
