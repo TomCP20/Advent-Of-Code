2610d8d561b9a5e9e5224f8b74be2b1efc9768fd
dei20 <107045601+TomCP20@users.noreply.github.com>
2025-12-10 17:12:04 +0000

    solved part 2 of day 10

--- 2025/Day 10/Python/main.py
+++ 2025/Day 10/Python/main.py
@@ -2,14 +2,17 @@
 
 import heapq
 
+import z3  # type: ignore
 
-def parse_line(line: str) -> tuple[int, list[list[int]]]:
+
+def parse_line(line: str) -> tuple[int, list[list[int]], list[int]]:
     """parses a line of input"""
-    diagram, *schematics, _ = line.split()
-    lights = (light == "#" for light in diagram[1:-1])
-    goal = sum(int(bit) << i for (i, bit) in enumerate(lights))
-    buttons: list[list[int]] = [list(map(int, b[1:-1].split(","))) for b in schematics]
-    return (goal, buttons)
+    diagram, *schematics, requirements = line.split()
+    positions = (light == "#" for light in diagram[1:-1])
+    lights = sum(int(bit) << i for (i, bit) in enumerate(positions))
+    buttons = [list(map(int, b[1:-1].split(","))) for b in schematics]
+    joltage = list(map(int, requirements[1:-1].split(",")))
+    return (lights, buttons, joltage)
 
 
 def dijkstra(buttons: list[list[int]]):
@@ -32,11 +35,39 @@ def dijkstra(buttons: list[list[int]]):
     return dist
 
 
+def solve_joltage(buttons: list[list[int]], joltage: list[int, ...]) -> int:  # type: ignore
+    """solves the number of buttons to press to reach the required joltage"""
+    solver = z3.Optimize()
+    variables: list[z3.ArithRef] = []
+    joltages_var: list[z3.ArithRef | None] = [None] * len(joltage)
+
+    for name, button in enumerate(buttons):
+        var = z3.Int(str(name))  # type: ignore
+        variables.append(var)
+        solver.add(var >= 0)  # type: ignore
+
+        for entry in button:
+            if joltages_var[entry] is None:
+                joltages_var[entry] = var
+            else:
+                joltages_var[entry] += var
+
+    for jolt_int, entry in enumerate(joltage):
+        if joltages_var[jolt_int] is not None:
+            solver.add(joltage[jolt_int] == joltages_var[jolt_int])  # type: ignore
+
+    total_presses = solver.minimize(sum(variables))  # type: ignore
+
+    assert solver.check() == z3.sat  # type: ignore
+    return total_presses.value().as_long()  # type: ignore
+
+
 def main():
     """Solves Part 1 and 2 of Day 10"""
     with open(0, encoding="utf-8") as f:
         machines = list(map(parse_line, f.read().splitlines()))
-    print(sum(dijkstra(buttons)[goal] for goal, buttons in machines))
+    print(sum(dijkstra(buttons)[lights] for lights, buttons, _ in machines))
+    print(sum(solve_joltage(buttons, joltage) for _, buttons, joltage in machines))
 
 
 main()
